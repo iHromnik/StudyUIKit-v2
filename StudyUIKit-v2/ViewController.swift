4463ba4c2075bcc280c7d1919127e3eec516a28a
Roman Khovalko <hromnik@me.com>
2024-02-06 15:42:01 +0200

    Initial Commit

---
+++ StudyUIKit-v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyUIKit-v2
+//
+//  Created by iHRomnik on 06.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
